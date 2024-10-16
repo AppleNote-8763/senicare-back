b6047d89f6389e937dd3959fe5385656049fa699
AppleNote-8763 <chldbstn132@naver.com>
2024-10-16 14:13:05 +0900

    first commit

---
+++ src/main/java/com/korit/senicare/repository/NurseRepository.java
@@ -0,0 +1,17 @@
+package com.korit.senicare.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.korit.senicare.entity.NurseEntity;
+
+@Repository
+public interface NurseRepository extends JpaRepository<NurseEntity, String> {
+
+    boolean existsByUserId(String userId);
+    boolean existsByTelNumber(String telNumber);
+
+    NurseEntity findByUserId(String userId);
+    NurseEntity findBySnsIdAndJoinPath(String sns, String joinPath);
+
+}
